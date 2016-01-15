96ac95256f4b64e3951ca50252ac1a3dc0d524b9
Daniel Suter <suter_daniel@hotmail.com>
2016-01-15 23:59:56 +0100

    path will be read from system properties

--- TorrentFeedProvider/src/main/java/ch/danielsuter/torrentfeedprovider/persistence/ConfigurationDao.java
+++ TorrentFeedProvider/src/main/java/ch/danielsuter/torrentfeedprovider/persistence/ConfigurationDao.java
@@ -4,18 +4,33 @@ import java.io.File;
 
 import javax.xml.bind.JAXB;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import ch.danielsuter.torrentfeedprovider.domain.Configuration;
 
 public class ConfigurationDao {
-	
+
+	private final static Logger logger = LoggerFactory.getLogger(ConfigurationDao.class);
 	private static final String CONFIGURATION_FILENAME = "configuration.xml";
-	
+
+	private File getLocation() {
+		String configPath = System.getProperty("configPath");
+		if (configPath == null) {
+			File file = new File(CONFIGURATION_FILENAME);
+			logger.warn("Property configPath is not defined, using default: {}", file.getAbsolutePath());
+			return file;
+		} else {
+			return new File(configPath, CONFIGURATION_FILENAME);
+		}
+	}
+
 	public Configuration readConfiguration() {
-		Configuration configuration = JAXB.unmarshal(new File(CONFIGURATION_FILENAME), Configuration.class);
+		Configuration configuration = JAXB.unmarshal(getLocation(), Configuration.class);
 		return configuration;
 	}
-	
+
 	public void write(Configuration configuration) {
-		JAXB.marshal(configuration, new File(CONFIGURATION_FILENAME));
+		JAXB.marshal(configuration, getLocation());
 	}
 }
